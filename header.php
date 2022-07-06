9f5a10d2a277cad7113c6d0ec443357cd716ff02
野崎翔太 <nozakishouta@nozakishoutanoMacBook-Pro.local>
2022-07-06 10:30:47 +0900

    大枠完成、スタイルと確認画面の調整必要

--- header.php
+++ header.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Engress</title>
+    <meta name=”description” content="日本人へのTOEFL指導歴豊かな講師陣のコーチング型TOEFLスクール" />
     <?php wp_head(); ?>
 </head>
 
